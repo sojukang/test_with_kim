72151b03695f23be9f139ef058c991ea7375cf4b
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-11-08 15:49:56 +0900

    Add class HelloWorld receive name. can print name by print

---
+++ test.py
@@ -0,0 +1,11 @@
+class HelloWorld():
+    def __init__(self, name):
+        self.name = name 
+    
+    def __str__(self):
+        return self.name 
+
+
+if __name__ == '__main__':
+    myName = HelloWorld('Kang')
+    print(myName)
